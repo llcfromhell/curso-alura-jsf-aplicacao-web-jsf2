95c3a9f9d8e16dcf678a30b84da25765cd01236f
llcfromhell <llc.from.hell@gmail.com>
2016-01-06 12:11:51 -0200

    Projeto com exercícios feitos
    Projeto já com exercícios executados

---
+++ livraria/src/TemaBean.java
@@ -0,0 +1,29 @@
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class TemaBean implements Serializable{
+
+    private String tema = "aristo";
+
+    public String getTema() {
+        return tema;
+    }
+
+    public void setTema(String tema) {
+        this.tema = tema;
+    }
+
+        //temas disponíveis dentro do JAR
+    public List<String> getTemas() {
+        return Arrays.asList("aristo", "black-tie", "blitzer", "bluesky", "casablanca", "cupertino", "dark-hive",
+                "dot-luv", "eggplant", "excite-bike", "flick", "glass-x", "hot-sneaks", "humanity", "le-frog",
+                "midnight", "mint-choc", "overcast", "pepper-grinder", "redmond", "rocket", "sam", "smoothness",
+                "south-street", "start", "sunny", "swanky-purse", "trontastic", "ui-darkness", "ui-lightness", "vader");
+    }   
+}
